60787105d8de47d278fd4f21dac60f3d51bf1bc9
İbrahim Kızılarslan <kizilarslanibrahim8@gmail.com>
2025-04-05 01:49:57 +0300

    entityler oluşturuldu

---
+++ Core/Hepsi.Domain/Entities/Brand.cs
@@ -0,0 +1,21 @@
+﻿using Hepsi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hepsi.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+
+        public Brand() { }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public required string Name { get; set; }
+
+    }
+}
